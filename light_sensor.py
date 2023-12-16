8db687608af5fcf85db253c6c41b103f50b172d0
JirR02 <jaye.ruh@icloud.com>
2023-12-16 18:31:03 +0100

    IDK

--- light_sensor.py
+++ light_sensor.py
@@ -5,19 +5,21 @@ GPIO.setmode(GPIO.BOARD)
 
 pinLDR = 7
 
-def rc_time (pinLDR):
+
+def rc_time(pinLDR):
     count = 0
     GPIO.setup(pinLDR, GPIO.OUT)
     GPIO.output(pinLDR, GPIO.LOW)
     time.sleep(0.1)
     GPIO.setup(pinLDR, GPIO.IN)
-    while (GPIO.input(pinLDR) == GPIO.LOW):
+    while GPIO.input(pinLDR) == GPIO.LOW:
         count += 1
     return count
 
+
 try:
     while True:
-        with open('Lightvalues_Static.txt', 'a') as f:
+        with open("Lightvalues_Static.txt", "a") as f:
             f.write(str(rc_time(pinLDR)))
             print(rc_time(pinLDR))
             time.sleep(300)
